ccbe0275610e7d0aa1660290fb0d47342be8feab
Shagun Raghav <75132564+shagunraghav@users.noreply.github.com>
2021-07-14 16:42:04 +0530

    DAy1 Assignment
    Assignment 1 Reactjs  LetsUpgrade

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from  "react"
+import "./App.css"
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+   <p>Java is to JavaScript what car is to Carpet.</p>
+   </div>
+  );
+}
+
+export default App;
